461435923efa8121fe243f8bb5fa1c3c5ec4403a
AliAyub007 <akhan.bscs15seecs@seecs.edu.pk>
2017-08-06 21:43:14 +0500

    Kubernetes deployment

---
+++ kubernetes/travis/deploy.sh
@@ -0,0 +1,28 @@
+export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
+
+export REPOSITORY="https://github.com/${TRAVIS_REPO_SLUG}.git"
+
+sudo rm -f /usr/bin/git-credential-gcloud.sh
+sudo rm -f /usr/bin/bq
+sudo rm -f /usr/bin/gsutil
+sudo rm -f /usr/bin/gcloud
+rm -rf node_modules
+
+curl https://sdk.cloud.google.com | bash;
+source ~/.bashrc
+gcloud components install kubectl
+
+gcloud config set compute/zone us-central1-b
+openssl aes-256-cbc -K $encrypted_473c9cce7549_key -iv $encrypted_473c9cce7549_iv -in ./kubernetes/travis/susi-telegrambot-e467bca1e540.json.enc -out susi-telegrambot-e467bca1e540.json -d
+mkdir -p lib
+gcloud auth activate-service-account --key-file susi-telegrambot-e467bca1e540.json
+export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/susi-telegrambot-e467bca1e540.json
+gcloud config set project susi-telegrambot
+gcloud container clusters get-credentials bots
+cd kubernetes/images/generator
+docker build --build-arg COMMIT_HASH=$TRAVIS_COMMIT --build-arg BRANCH=$DEPLOY_BRANCH --build-arg REPOSITORY=$REPOSITORY --no-cache -t aliayubkhan/susi_slackbot:$TRAVIS_COMMIT .
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker tag aliayubkhan/susi_slackbot:$TRAVIS_COMMIT aliayubkhan/susi_slackbot:latest-$DEPLOY_BRANCH
+docker push aliayubkhan/susi_slackbot
+kubectl set image deployment/susi-viberbot --namespace=slackbot susi-slackbot=aliayubkhan/susi_slackbot:$TRAVIS_COMMIT
+rm -rf $GOOGLE_APPLICATION_CREDENTIALS
