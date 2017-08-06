3bfe417a5eccd27506968141d6af239687aa6a9c
AliAyub007 <akhan.bscs15seecs@seecs.edu.pk>
2017-08-06 22:18:53 +0500

    Kubernetes deployment

--- kubernetes/travis/deploy.sh
+++ kubernetes/travis/deploy.sh
@@ -2,7 +2,7 @@ export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
 
 export REPOSITORY="https://github.com/${TRAVIS_REPO_SLUG}.git"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/susi_hangoutbot" -o  "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "AliAyub007/susi_hangoutbot" -o  "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "Skip production deployment for a very good reason."
     exit 0
 fi
